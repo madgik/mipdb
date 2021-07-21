e982eb3bd682e02eeed84b8b9d6b44b5853dfb0e
Jason Sakellariou <js.sakel@gmail.com>
2021-07-21 10:52:20 +0300

    Wait until monetdb container is ready before running tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,77 +12,34 @@ def schema_data():
     schema_file = "tests/data/schema.json"
     reader = JsonFileReader(schema_file)
     return reader.read()
-    # return {
-    #     "code": "schema",
-    #     "label": "The Schema",
-    #     "version": "1.0",
-    #     "variables": [
-    #         {
-    #             "isCategorical": False,
-    #             "code": "var1",
-    #             "sql_type": "text",
-    #             "description": "",
-    #             "label": "",
-    #             "methodology": "",
-    #         },
-    #         {
-    #             "isCategorical": False,
-    #             "code": "var2",
-    #             "sql_type": "text",
-    #             "description": "",
-    #             "label": "",
-    #             "methodology": "",
-    #         },
-    #     ],
-    #     "groups": [
-    #         {
-    #             "name": "group",
-    #             "label": "The Group",
-    #             "variables": [
-    #                 {
-    #                     "isCategorical": False,
-    #                     "code": "var3",
-    #                     "sql_type": "text",
-    #                     "description": "",
-    #                     "label": "",
-    #                     "methodology": "",
-    #                 },
-    #             ],
-    #             "groups": [
-    #                 {
-    #                     "name": "inner_group",
-    #                     "label": "The Inner Group",
-    #                     "variables": [
-    #                         {
-    #                             "isCategorical": False,
-    #                             "code": "var4",
-    #                             "sql_type": "text",
-    #                             "description": "",
-    #                             "label": "",
-    #                             "methodology": "",
-    #                         },
-    #                     ],
-    #                 }
-    #             ],
-    #         }
-    #     ],
-    # }
+
+
+class MonetDBSetupError(Exception):
+    """Raised when the MonetDB container is unable to start."""
 
 
 @pytest.fixture(scope="session")
 def monetdb_container():
     client = docker.from_env()
     try:
-        client.containers.get("mipdb-testing")
+        container = client.containers.get("mipdb-testing")
     except docker.errors.NotFound:
-        client.containers.run(
+        container = client.containers.run(
             "madgik/mipenginedb:dev1.4",
             detach=True,
             ports={"50000/tcp": "50123"},
             name="mipdb-testing",
             publish_all_ports=True,
         )
-    time.sleep(0.5)  # Container needs to sleep to get its shit together
+    # The time needed to start a monetdb container varies considerably. We need
+    # to wait until some phrase appear in the logs to avoid starting the tests
+    # too soon. The process is abandoned after 100 tries (50 sec).
+    for _ in range(100):
+        if b"new database mapi:monetdb" in container.logs():
+            break
+        time.sleep(0.5)
+    else:
+        raise MonetDBSetupError
     yield
     container = client.containers.get("mipdb-testing")
     container.remove(v=True, force=True)
